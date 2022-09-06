b72f14f9d9bb6f7158d2152b9a0ed57d6b7a1f55
hoang nam <namhvth2204008@fpt.edu.vn>
2022-09-07 01:48:54 +0700

    1

--- doanAGL/src/app/Components/cpn-header/cpn-header.component.ts
+++ doanAGL/src/app/Components/cpn-header/cpn-header.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CpnHeaderComponent implements OnInit {
 
-  constructor() { }
+  array =[1,2,4,2,2]
+
+  constructor() {
+    const list = document.querySelectorAll('.list');
+    list.forEach(item=>{
+          item.addEventListener('click',()=>{
+            console.log('1')
+            });
+          })
+    // console.log(list)
+    // function activeLink(){
+    //     list.forEach((item) =>
+    //     item.classList.remove('active'));
+    //     this.activeLink.classList.add('active')
+    // }
+    // list.forEach((item) =>
+    // item.addEventListener('click',activeLink));
+
+
+  //   list.forEach(item=>{
+  //     item.addEventListener('click',()=>{
+  //       list.forEach(item2 =>{
+  //         item2.classList.remove('active');
+  //       });
+  //   });
+  //   item.classList.add('active');
+  // })
+  }
 
   ngOnInit(): void {
+
   }
 
 }
