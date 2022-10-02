b63de7b47f553c93de058f37f42dd2bd4d2cd78c
hoang nam <namhvth2204008@fpt.edu.vn>
2022-10-02 16:50:34 +0700

    1

--- doanAGL/src/app/Components/cpn-booking/cpn-booking.component.ts
+++ doanAGL/src/app/Components/cpn-booking/cpn-booking.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CpnBookingComponent implements OnInit {
   public isCollapsed = false;
+  public isCollapsed1 = true;
+  public isCollapsed2 = true;
+  public isCollapsed3 = true;
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+  collapsetoggle(index:any){
+    if(index==0){
+      this.isCollapsed=  this.isCollapsed=false?true:false;
+      this.isCollapsed1=true;this.isCollapsed2=true;this.isCollapsed3=true
+    }else if(index==1){
+      this.isCollapsed1=  this.isCollapsed1=false?true:false;
+      this.isCollapsed=true;this.isCollapsed2=true;this.isCollapsed3=true
+    }else if(index==2){
+      this.isCollapsed2=  this.isCollapsed2=false?true:false;
+      this.isCollapsed=true;this.isCollapsed1=true;this.isCollapsed3=true
+    }else{
+      this.isCollapsed3=  this.isCollapsed3=false?true:false;
+      this.isCollapsed=true;this.isCollapsed1=true;;this.isCollapsed2=true
+    }
   }
 
 }
